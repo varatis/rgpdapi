72f405cfb4c5af604f94a6dda519860d57d98898
GAUTIER Jérôme <jerome.gautier@groupe-creative.fr>
2026-05-21 06:40:05 +0000

    "CI/CD: Fix sonar, trivy stages"

--- .platforms/ci/sonar.sh
+++ .platforms/ci/sonar.sh
@@ -4,9 +4,7 @@
 source .env
 source .platforms/ci/bootstrap.sh
 
-echo "WORKDIR SONAR : ${WORKDIR}"
-
 # Lancer une conteneur qui va analyser le code du projet localement et ensuite envoyer les résultats au serveur SonarQube
 # Analyse du code back
-BACK_SONAR_IMAGE_EXTRA_OPTS="-v ${WORKSPACE}:${WORKDIR}"
+BACK_SONAR_IMAGE_EXTRA_OPTS="-v ${WORKSPACE}:${WORKDIR} -e sonar.branch.name=${BRANCH_NAME}"
 dockerRun "sonar-scanner" "${WORKDIR}" "${BACK_SONAR_IMAGE_EXTRA_OPTS}" "srv-nexus.domaine.local:18443/outillage/sonarqube-scanner:7.2" "sonar-scanner"
