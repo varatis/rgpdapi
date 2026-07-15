a6a1d9f8b7e8b7cde523ec4f6f1051bc2c714c20
Jérôme GAUTIER <jerome.gautier@groupe-creative.fr>
2026-07-15 14:44:45 +0200

    Revert echo env

--- .platforms/k8s/bootstrap-k8s-minds.sh
+++ .platforms/k8s/bootstrap-k8s-minds.sh
@@ -7,7 +7,5 @@
 #ENV_KUBECONFIG="-e KUBECONFIG=$HOME/.kube/kubeconfig-interne-admin"
 MY_KUBECONFIG="--kubeconfig=$KUBECONFIG"
 
-cat $KUBECONFIG
-
 # Récupération du nom du pod "tools-cli" si nécessaire
 TOOLS_CLI_POD_NAME=`kubectl $MY_KUBECONFIG -n minds get pods --selector=app.kubernetes.io/name=tools-cli --output=jsonpath={.items.metadata.name}`
